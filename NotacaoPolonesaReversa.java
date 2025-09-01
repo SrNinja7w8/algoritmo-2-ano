ef1d56978153308936ba8d347ab953a8bf3d825b
SrNinja7w8 <ezequielwanzelerdeoliveira@gmail.com>
2025-09-01 09:51:05 -0300

    NotacaoPolonesaReversa.java

--- NotacaoPolonesaReversa.java
+++ NotacaoPolonesaReversa.java
@@ -1,33 +1,33 @@
 import java.util.Stack;
 import javax.swing.JOptionPane;
-public class atv {
+public class atvv {
     public static void main(String[] args) {
-        Stack<Character> P1 = new Stack<>();
-        Stack<Character> P2 = new Stack<>();
-
-        String poli = JOptionPane.showInputDialog("Digite a palavra: ");
-        poli = poli.toLowerCase();
-        char[] pol = poli.toCharArray();
-          for(char c : pol) {
-            P1.push(c);
+        Stack<Character> ntcs = new Stack<>();
+        String ntc = JOptionPane.showInputDialog("Digite a notação polonesa reversa: ");
+        for (int i = 0; i < ntc.length(); i++) {
+            char c = ntc.charAt(i);
+            if (Character.isDigit(c)) {
+                ntcs.push(c);
+            } else {
+                int b = Character.getNumericValue(ntcs.pop());
+                int a = Character.getNumericValue(ntcs.pop());
+                int resultado = 0;
+                switch (c) {
+                    case '+':
+                        resultado = a + b;
+                        break;
+                    case '-':
+                        resultado = a - b;
+                        break;
+                    case '*':
+                        resultado = a * b;
+                        break;
+                    case '/':
+                        resultado = a / b;
+                        break;
+                }
+                ntcs.push((char) (resultado + '0'));
+            }
         }
-          for(char c : pol) {
-            P1.push(c);
-        }
-        
-        for(int i=0; i<P1.size(); i++) {
-            P2.push(P1.pop());
-        }
-
-
-
-
-        if(P1.equals(P2)) {
-            JOptionPane.showMessageDialog(null, "A palavra é um palíndromo!");
-        } else {
-            JOptionPane.showMessageDialog(null, "A palavra não é um palíndromo!");
-        }
-
-        JOptionPane.showMessageDialog(null, "Pilha: " + P1 + "\n" + "Pilha Invertida: " + P2);
     }
 }
